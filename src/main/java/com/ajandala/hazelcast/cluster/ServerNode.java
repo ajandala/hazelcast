f02dc81eca7ffd7863d9436fddb922ee74fc1b00
ajandala <ajandala.ext@hotelbeds.com>
2019-06-04 18:10:33 +0200

    pom ServerNode

---
+++ src/main/java/com/ajandala/hazelcast/cluster/ServerNode.java
@@ -0,0 +1,10 @@
+package com.ajandala.hazelcast.cluster;
+
+public class ServerNode {
+
+    public static void main(String[] args) {
+        HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
+    }
+
+
+}
